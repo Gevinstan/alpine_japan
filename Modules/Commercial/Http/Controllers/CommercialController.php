aaadb5ea12831776f86e1ab39656c59a691f5edd
Vigneshbaraman <vbjr317@gmail.com>
2024-10-27 11:58:57 +0530

    images change work

--- Modules/Commercial/Http/Controllers/CommercialController.php
+++ Modules/Commercial/Http/Controllers/CommercialController.php
@@ -80,7 +80,7 @@ class CommercialController extends Controller
           $org_filename = $model_image->getClientOriginalName();
           $org_extension = $model_image->getClientOriginalExtension();
           $image_name = 'small-heavy-'.date('-Y-m-d-h-i-s-').rand(999,9999).'.'.$org_extension;
-          $org_path = public_path() . '/Small-Heavy';
+          $org_path = public_path() . '/Cars';
           File::isDirectory($org_path) or File::makeDirectory($org_path, 0777, true, true);
           $model_image->move($org_path, $image_name);
         } else {
