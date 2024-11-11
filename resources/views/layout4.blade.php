061642c7128f2efe69003cbdce42f2d45e5b38de
vineshbaraman <vbjr317@gmail.com>
2024-11-11 12:20:57 +0530

    index 4 modification work

--- resources/views/layout4.blade.php
+++ resources/views/layout4.blade.php
@@ -489,19 +489,6 @@
 
 
 
-    @if ($tawk_chat->status == 1)
-        <script type="text/javascript">
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='{{ $tawk_chat->chat_link }}';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-            })();
-        </script>
-    @endif
 
 
     <!-- footer prart start  -->
