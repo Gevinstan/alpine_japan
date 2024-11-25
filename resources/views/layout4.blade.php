9718dc43ac1992ad0b6f007156c36d7561cb7d29
Abineshfullstack11 <abichampier@gmail.com>
2024-11-25 10:58:51 +0530

    jdm stock and about style changes

--- resources/views/layout4.blade.php
+++ resources/views/layout4.blade.php
@@ -543,9 +543,9 @@
     <!-- footer prart start  -->
 
     @if (Route::is('home'))
-    <footer class="footer {{ Session::get('selected_theme') == 'theme_two' ? 'footer-three' : ''  }} {{ Session::get('selected_theme') == 'theme_one' ? 'footer-two' : ''  }}">
+    <footer class=" footer {{ Session::get('selected_theme') == 'theme_two' ? 'footer-three' : ''  }} {{ Session::get('selected_theme') == 'theme_one' ? 'footer-two' : ''  }}">
     @else
-    <footer class="footer footer-two">
+    <footer class="overflow-hidden footer footer-two">
     @endif
 
         <div class="container px-2 px-sm-3 px-lg-5">
