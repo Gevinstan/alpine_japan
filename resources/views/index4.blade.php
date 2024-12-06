3b0350a8a2743de4d56e1b933723bad922cb8920
vineshbaraman <vbjr317@gmail.com>
2024-12-06 15:56:31 +0530

    Trim funcitnality issue found

--- resources/views/index4.blade.php
+++ resources/views/index4.blade.php
@@ -378,7 +378,7 @@ use Illuminate\Support\Str;
                                                         <p>
                                                             <i class="bi bi-geo-alt-fill fs-6"></i>
                                                             <span class="brand-location">
-                                                            {{ isset($parsed_data['vehicle  location']) ? trim(Str::limit($parsed_data['vehicle  location'], 10, '...')) : '--' }}
+                                                            {{ isset($parsed_data['vehicle  location']) ? trim($parsed_data['vehicle  location']) : '--' }}
                                                             </span>
                                                         </p>
                                                         <div class="d-flex flex-column">
@@ -591,7 +591,7 @@ use Illuminate\Support\Str;
 
                                                         <p>
                                                             <i class="bi bi-geo-alt-fill fs-6"></i>
-                                                            <span class="brand-location">{{Str::limit($car->location, 10, '...')}}</span>
+                                                            <span class="brand-location">{{$car->location}}</span>
                                                         </p>
                                                         <div class="d-flex flex-column">
                                                             <span class="brand-date fw-light">{{ $carbonInstance->format('Y-m-d') }}</span>
@@ -844,7 +844,7 @@ use Illuminate\Support\Str;
                                             @endphp
                                             <p>
                                                 <i class="bi bi-geo-alt-fill fs-6"></i>
-                                                <span class="brand-location">  {{ isset($parsed_data['vehicle  location']) ? trim(Str::limit($parsed_data['vehicle  location'], 10, '...')) : '--' }}</span>
+                                                <span class="brand-location"> {{ isset($parsed_data['vehicle  location']) ? trim($parsed_data['vehicle  location']) : '--' }}</span>
                                             </p>
                                             <div class="d-flex flex-column">
                                                 <span class="brand-date fw-light">{{ $carbonInstance->format('Y-m-d') }}</span>
