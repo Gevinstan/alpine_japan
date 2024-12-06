c5b8d9ff5d8a8d9d3ad1ace18798c02a7969656a
abdul-finiteloop <abdul.aziz@finiteloop.io>
2024-12-06 12:20:48 +0530

    Revert "remove testing command on homecontroller"
    This reverts commit 24bc5ed54af1faddc520c544cb2de41282636328.

--- resources/views/layout4.blade.php
+++ resources/views/layout4.blade.php
@@ -911,7 +911,7 @@
              // back to top start
     // top to bottom start
    
-    $(document).ready(function () {
+ $(document).ready(function () {
         // Back to Top Button
         $(".back-to-top").on("click", function () {
             $("html, body").animate(
