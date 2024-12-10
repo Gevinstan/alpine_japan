7650ba549fac3f315498ef11583fc9694ff1a60f
abdul-finiteloop <abdul.aziz@finiteloop.io>
2024-12-10 13:16:04 +0530

    auction car listing page style changes done

--- resources/views/car-listing-details.blade.php
+++ resources/views/car-listing-details.blade.php
@@ -156,11 +156,11 @@
                                                     </div>
                                                 </div>
 
-                                                @if($google_recaptcha->status==1)
+                                                {{-- @if($google_recaptcha->status==1)
                                                     <div class="auto-sales-form-item">
                                                         <div class="g-recaptcha" data-sitekey="{{ $google_recaptcha->site_key }}"></div>
                                                     </div>
-                                                @endif
+                                                @endif --}}
                                                 <input type="hidden" name="car_id" value="{{$car->id}}">
                                                 <input type="hidden" name="commission" value="" id="hidden_commission">
                                                 <input type="hidden" name="delivery_charge" value="" id="hidden_delivery_charge">
