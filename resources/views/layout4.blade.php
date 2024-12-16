7d69546d26074cb1d24613f19e241386a1fe7fb0
vineshbaraman <vbjr317@gmail.com>
2024-12-16 12:53:43 +0530

    hide useful links

--- resources/views/layout4.blade.php
+++ resources/views/layout4.blade.php
@@ -632,7 +632,7 @@
                                                     </svg></span> {{ __('translate.Our Stocks') }}
                                             </a>
                                         </li>
-                                        <li>
+                                        {{--<li>
                                             <a href="{{ route('custom-page', 'useful-links') }}" class="{{Route::is('custom-page') && request()->segment(2) == 'useful-links' ? 'active' : '' }}"> <span>
                                             <svg width="13" height="10" viewBox="0 0 13 10" fill="none"
                                                         xmlns="http://www.w3.org/2000/svg">
@@ -641,7 +641,7 @@
                                                             stroke-linecap="round" stroke-linejoin="round" />
                                                     </svg></span> {{ __('translate.Useful Links') }}
                                             </a>
-                                        </li>
+                                        </li>--}}
 
 
                                 </ul>
