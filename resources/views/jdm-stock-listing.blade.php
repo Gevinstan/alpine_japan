b58520944088ea8e089a0487a6009c4a9f25c510
vigneshbaraman <vbjr317@gmail.com>
2025-03-17 18:27:08 +0530

    jdm-calculation isue fixing

--- resources/views/jdm-stock-listing.blade.php
+++ resources/views/jdm-stock-listing.blade.php
@@ -100,26 +100,26 @@
                             </label>
                         </div>
                         <div class="form-check ">
-                            <input class="form-check-input" type="radio" name="shipment" value="1">
-                            <label class="form-check-label ps-2" for="marine_insurance">
+                            <input class="form-check-input" type="radio" name="shipment1" value="1">
+                            <label class="form-check-label ps-2">
                                 RoRo
                             </label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="shipment" value="2">
+                            <input class="form-check-input" type="radio" name="shipment1" value="2">
                             <label class="form-check-label ps-2">
                                 Container 
                             </label>
                         </div>  
                     </div> 
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox"  id="marine_insurance" name="marine_insurance">
+                        <input class="form-check-input" type="checkbox"  id="marine_insurance1" name="marine_insurance">
                         <label class="form-check-label ps-2" for="marine_insurance">
                             Marine insurance 
                         </label>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox"  id="inland_inspection" name="inland_inspection">
+                        <input class="form-check-input" type="checkbox"  id="inland_inspection1" name="inland_inspection">
                         <label class="form-check-label ps-2" for="inland_inspection">
                             Inland inspection 
                         </label>
@@ -866,14 +866,14 @@
                 return item.id == delivery_location;
             });
 
-            if ($("#marine_insurance").is(':checked')) {
+            if ($("#marine_insurance1").is(':checked')) {
                 // var delivery_charge_num = Number(shipment); 
                  var marine_insurance={{ round($car_charges->marine_insurance_value) }}
             } else {
                 var marine_insurance=0;
             }
  
-            if ($("#inland_inspection").is(':checked')) {
+            if ($("#inland_inspection1").is(':checked')) {
                 var inland_inspection={{ round($car_charges->inland_inspection_value) }}
             } else {
                 var inland_inspection=0;
@@ -882,9 +882,9 @@
             var shipment=0;
             
             if(filtered_charge.length > 0){
-                if ($("input[name='shipment']:checked").val() === "1") {
+                if ($("input[name='shipment1']:checked").val() === "1") {
                     shipment =filtered_charge[0].roro;
-                } else if ($("input[name='shipment']:checked").val() === "2") {
+                } else if ($("input[name='shipment1']:checked").val() === "2") {
                     // Container is selected
                     shipment =filtered_charge[0].container;
                 } else {
@@ -907,7 +907,7 @@
                 
                  // Calculate total
                 //  var total_price = start_price_num +  delivery_charge_num + comission_price_num  +shipping_charge_num;
-                 var total_price = start_price_num +  delivery_charge_num;
+                var total_price = start_price_num + delivery_charge_num+marine_insurance+inland_inspection;
  
                 //  $("#hidden_commission").val(comission_price_num);
                 //  $("#hidden_delivery_charge").val(delivery_charge_num);
